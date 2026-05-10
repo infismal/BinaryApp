4ec8c406f682a77eec0c37eb44641fdb2c168270
Pete <koala2324@outlook.com>
2026-05-09 23:21:33 -0500

    desmadre____arreglateDesmadregit add .git add .!

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Probando cositas");
+
+        String s;
+
+        //s = DecToBaseConverter.convert(25, 2);
+
+        s = DecToBaseConverter.convertToHex(646543);
+
+        System.out.println(s);
     }
 }
 

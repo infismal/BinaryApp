ff24be3a87b47d6bc3f44ada75d81281cca15b59
Pete <koala2324@outlook.com>
2026-05-09 22:43:25 -0500

    guardateeeeee

---
+++ src/DecToBaseConverter.java
@@ -0,0 +1,35 @@
+public class DecToBaseConverter 
+{
+  public static String convert(int dec, int base)
+  {
+    String str = "";
+    while (dec >= base) 
+    {
+      str = dec%base + str;
+      dec = dec/base;
+    }
+
+    str = dec + str;
+    return str;
+  }
+  
+  public  static  String convertToHex(int dec)
+  {
+    String str = "";
+    int rema;
+    while (dec >= 16) 
+    {
+      rema = dec%16;
+      if(rema >= 10)
+      {
+        str = Hex.getLetter(rema) + str;
+      }else{str = rema + str;}
+
+      dec = dec/16;
+    }
+    str = dec + str;
+    return str;
+  }
+
+}
+
---
+++ src/Hex.java
@@ -0,0 +1,32 @@
+
+
+public enum Hex 
+{
+  A(10),
+  B(11),
+  C(12),
+  D(13),
+  E(14),
+  F(15);
+
+  private final int value;
+
+  Hex(int value) 
+  {
+      this.value = value;
+  }
+
+  public int getValue(){return value;}
+
+  // Método para obtener el nombre (A, B, C...) según el número
+  public static String getLetter(int number) {
+      for (Hex hd : Hex.values()) {
+          if (hd.getValue() == number) {
+              return hd.name();
+          }
+      }
+      return String.valueOf(number); // Retorna el número si es 0-9
+  }
+  
+}
+
